895d6d4a3a32b58128a535d2ba1d4ffa8c1d2c30
Yunsheng Tian <knoxtian@icloud.com>
2019-03-14 23:07:56 +0800

    change default configuration

--- rlkit/launchers/launcher_util.py
+++ rlkit/launchers/launcher_util.py
@@ -173,9 +173,9 @@ def create_exp_name(exp_prefix, exp_id=0, seed=0):
     :param exp_id:
     :return:
     """
-    now = datetime.datetime.now(dateutil.tz.tzlocal())
-    timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
-    return "%s_%s_%04d--s-%d" % (exp_prefix, timestamp, exp_id, seed)
+    # now = datetime.datetime.now(dateutil.tz.tzlocal())
+    # timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
+    return "%s_%s_s-%d" % (exp_prefix, exp_id, seed)
 
 
 def create_log_dir(
